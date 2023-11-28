cd0c014c460defab6a6446b50de683b50f136a45
Petrik Diak <diak@petrik.hu>
2023-11-28 13:43:35 +0100

    hopethisworks

---
+++ src/Registration.dto.ts
@@ -0,0 +1,6 @@
+export class RegistrationDto {
+  email: string;
+  password: string;
+  password_again: string;
+  age: string;
+}
--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,60 @@
-import { Controller, Get, Render } from '@nestjs/common';
+import { Body, Controller, Get, Post, Render, Res } from '@nestjs/common';
 import { AppService } from './app.service';
-
+import { RegistrationDto } from './registration.dto';
+import { Response } from 'express';
+import { User } from './user';
+const users: User[] = [new User('admin@example.com', 'asdf1234', 23)];
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
+  @Get('register')
+  @Render('registerForm')
+  registerForm() {
+    return { errors: [] };
+  }
   @Get()
   @Render('index')
   index() {
     return { message: 'Welcome to the homepage' };
+  }
+  @Post('register')
+  @Render('registerForm')
+  register(@Body() registration: RegistrationDto, @Res() res: Response) {
+    const errors: string[] = [];
+    if (!registration.email.includes('@')) {
+      errors.push('Az email formátum nem megfelelő');
+    }
+    if (registration.password.length < 8) {
+      errors.push('A jelszó túl rövid');
+    }
+    if (registration.password !== registration.password_again) {
+      errors.push('A két jelszó nem egyezik');
+    }
+    if (parseInt(registration.age) <= 0) {
+      errors.push('Az életkor nem megfelelő');
+    }
+    if (registration.age == '') {
+      errors.push('Adja meg az életkort');
+    }
+    if (errors.length > 0) {
+      return {
+        errors,
+      };
+    } else {
+      users.push(
+        new User(
+          registration.email,
+          registration.password,
+          parseInt(registration.age),
+        ),
+      );
+      console.log(users);
+      res.redirect('/');
+    }
+  }
+  @Post('login')
+  login(): string {
+    return 'You are logged!';
   }
 }
